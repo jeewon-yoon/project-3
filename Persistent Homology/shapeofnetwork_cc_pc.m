fbce9416a70879aedd5c09c498c2911fd40c16c0
jeewon_yoon <117123945+jeewon-yoon@users.noreply.github.com>
2023-01-24 21:08:36 -0600

    update

--- Persistent Homology/shapeofnetwork_cc_pc.m
+++ Persistent Homology/shapeofnetwork_cc_pc.m
@@ -8,15 +8,16 @@ function [MST, A, Dx, Cx] = shapeofnetwork_cc_pc(X,VAR)
 % Dx : p*p single linkage matrix %(sqrt)HSG
 % Cx : p*p distance matrix %(sqrt)HSG 
 
-[n p] = size(X); %[p n]= size(X); Edit hkang 2015.01.02
+[n p] = size(X); %[p n]= size(X); 
 
-%C = partialcorr(X,VAR) % C = corrcoef(X);  %C=partialcorr(X',VAR);
-C = corr(X,'Type','Pearson');  %C=partialcorr(X',VAR); %C = corr(X, 'Type','Spearman');
-% 혜경선생님 원래 코드 C = corrcoef(X');
-% C = corrcoef(X');
+%C = partialcorr(X,VAR)  %C = corrcoef(X);  %C=partialcorr(X',VAR);
+C = corr(X,'Type','Pearson');  
+%C = partialcorr(X',VAR); 
+%C = corr(X,'Type','Spearman');
+%C = corrcoef(X');
 
-C = C.*(C>0); % 새로만들었음. positive matrix만 찾아서 distance를 구하라는 코드
-Cx = sqrt(1 - C); % sqrt 추가. 2017.9.14 HSG
+C = C.*(C>0); % Find only the positive matrix and compute the distance
+Cx = sqrt(1 - C); % added sqrt command
 
 [Tree, pred] = graphminspantree(sparse(Cx),'Method','Kruskal');
 [row col] = find(Tree);
@@ -27,5 +28,5 @@ MST = MST(tind,:);
 A = full(Tree);
 % Dx = single_linkage_distance(MST,size(Cx,1));
 Dx = single_linkage_distance(Cx); 
-% 20120226
+
 
